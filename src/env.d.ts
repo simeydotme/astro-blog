f153ddceb4ef56cc0e41158358a57ef597ec0ce7
simeydotme <simey.me@gmail.com>
2023-09-11 01:44:19 +0800

    add vercel analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+export {};
+
+declare global {
+  interface Window {
+    va: function;
+    vaq: function;
+  }
+}
